33af8ad2c64a495c066c664588076c67b39d754a
arledso <121693910+arledso@users.noreply.github.com>
2025-07-26 01:47:12 -0300

    update font

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,14 @@ use relm4::RelmApp;
 
 fn main() {
     let app = RelmApp::new("relm4.test.simple");
+    relm4::set_global_css("
+        * {
+            font-family: 'FiraCode Nerd Font Mono';
+            font-size: 12px;
+        }
+        label {
+            font-weight: light;
+        }
+    ");
     app.run::<BarModel>(BarModel { date_string: String::from("00:00am"), time_string: String::from("...")});
 }
