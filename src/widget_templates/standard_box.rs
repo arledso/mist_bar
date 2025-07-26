911725584fe1f8a0192b6cbcdf3386ddee5690be
arledso <121693910+arledso@users.noreply.github.com>
2025-07-26 01:48:30 -0300

    update standard box border radius

--- src/widget_templates/standard_box.rs
+++ src/widget_templates/standard_box.rs
@@ -5,7 +5,7 @@ use relm4::gtk::prelude::*;
 impl WidgetTemplate for StandardBox {
     view! {
         gtk::Box {
-            inline_css: "border: 0px solid blue; background: #323027; background: linear-gradient(175deg,rgba(50, 48, 39, 1) 0%, rgba(60, 56, 54, 1) 100%); width: 10px; border-radius: 10px;",
+            inline_css: "border: 0px solid blue; background: #323027; background: linear-gradient(175deg,rgba(50, 48, 39, 1) 0%, rgba(60, 56, 54, 1) 100%); width: 10px; border-radius: 5px;",
             set_width_request: 90,
         }, 
     }
